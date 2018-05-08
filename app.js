a69efa59e4a69ab07feb4d9bd6c3c63088b1f527
bousquetb63 <jbousque@asu.edu>
2018-05-07 22:47:37 -0700

    improved bot and shortened code

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const Discord = require('discord.js');
 const settings = require('./settings.json');
 const bot = new Discord.Client({disableEveryone: true});
-const prefix = '!';
 bot.on('ready', async () => {
   console.log(`${bot.user.username} is Reporting for duty!`);
-  bot.user.setActivity(`${prefix}help for list of cmds`)
+  bot.user.setActivity(`!roles for list of roles`)
   try {
       let link = await bot.generateInvite(["ADMINISTRATOR"]);
       console.log(link);
@@ -14,21 +13,30 @@ bot.on('ready', async () => {
 });
 
 bot.on('message', message => {
-	if (message.channel.type === 'dm') return;
-    if (message.content.includes(prefix + 'gamer')){
-        let gamerRole = message.guild.roles.find("name", "Gamer");
-        let cryptoRole = message.guild.roles.find("name", "Crypto");
-        if(!message.member.roles.has(gamerRole.id) && !message.member.roles.has(cryptoRole.id)){
-            message.reply('You have been given crypto role!');
-			message.guild.member(message.author).addRole(message.guild.roles.find("name", "Gamer"));
+    if (message.channel.type === 'dm' || !message.content.includes('!')) return;
+    let gamerRole = message.guild.roles.find("name", "Gamers");
+    let cryptoRole = message.guild.roles.find("name", "Crypto");
+    var splitCMD = message.content.split('!');
+    if (message.member.roles.has(gamerRole.id) || message.member.roles.has(cryptoRole.id)) {
+        if (message.content.includes("gamer") || message.content.includes("crypto")) {
+            message.reply("You have already picked a role!");
+            return;
         }
-    } else if (message.content.includes(prefix + 'crypto')){
-        let gamerRole = message.guild.roles.find("name", "Gamer");
-        let cryptoRole = message.guild.roles.find("name", "Crypto");
-        if(!message.member.roles.has(gamerRole.id) && !message.member.roles.has(cryptoRole.id)){
-            message.reply('You have been given gamer role!');
-			message.guild.member(message.author).addRole(message.guild.roles.find("name", "Crypto"));
-        }
+    }
+    switch (splitCMD[1]) {
+        case "roles":
+            message.reply('Use !gamer or !crypto to be assigned a role.');
+            return;
+        case "gamer":
+            message.reply('You have been given the gamer role!');
+            message.guild.member(message.author).addRole(gamerRole);
+            return;
+        case "crypto":
+            message.reply('You have been given the crypto role!');
+            message.guild.member(message.author).addRole(cryptoRole);
+            return;
+        default:
+            return;
     }
 });
 // login bot with token
